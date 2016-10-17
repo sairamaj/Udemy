6321098af226e6c7ff7c8f5bb380d1135e6622ac
sairama <sairamaj@hotmail.com>
2016-10-16 17:44:30 -0700

    If

---
+++ Chap9(If)/Application.java
@@ -0,0 +1,12 @@
+class Application{
+	public static void main(String[] args){
+		int i=10;
+		if( i<10 ){
+			System.out.println("i is less than 10!");
+		}else if( i==10){
+			System.out.println("i is equal to 10! ");
+		}else{
+			System.out.println("I is more than 10 ");
+		}
+	}
+}
