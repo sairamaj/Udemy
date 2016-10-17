30e5d28b76e16c40664b01c55ca4e2dfb8d6c9f8
sairama <sairamaj@hotmail.com>
2016-10-16 18:04:47 -0700

    DoWhile

---
+++ Chap12(DoWhile)/Application.java
@@ -0,0 +1,12 @@
+class Application{
+	public static void main(String[] args){
+		/*
+			multi line comments
+		*/
+		int i=1;
+		do{
+			i++;
+			System.out.println("i is:" + i);
+		}while(i<10);
+	}
+}
