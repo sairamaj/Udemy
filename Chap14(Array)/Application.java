74caa13a63dea12c12510bac0de0ead6cacf19dd
sairama <sairamaj@hotmail.com>
2016-10-17 21:09:49 -0700

    Arrays

---
+++ Chap14(Array)/Application.java
@@ -0,0 +1,18 @@
+class Application{
+	public static void main(String[] args){
+		int[] vals;
+		vals = new int[3];
+		vals[0] = 10;
+		vals[1] = 20;
+		vals[2] = 30;
+
+		for(int i=0; i<=2; i++){
+			System.out.println(vals[i]);
+		}
+
+		int[] nums = {5,6,7};
+		for(int i=0; i<=2; i++){
+			System.out.println(nums[i]);	
+		}
+	}
+}
