f08e684922c00faded6d5786f75b5dbbdaccfbbd
sairama <sairamaj@hotmail.com>
2016-10-17 21:12:53 -0700

    String Arrays

---
+++ Chap15(StringArray)/Application.java
@@ -0,0 +1,12 @@
+class Application{
+	public static void main(String[] args){
+		String[] strs = new String[3];
+		strs[0] = "string_000";
+		strs[1] = "string_111";
+		strs[2] = "string_222";
+
+		for(String str: strs){
+			System.out.println(str);
+		}
+	}
+}
