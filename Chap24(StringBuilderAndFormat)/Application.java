b716776d471e5331b798aa9d057cdcccf85a8373
sairama <sairamaj@hotmail.com>
2016-11-05 14:22:20 -0700

    StringBuilder and Formatting

---
+++ Chap24(StringBuilderAndFormat)/Application.java
@@ -0,0 +1,11 @@
+public class Application{
+	public static void main(String[] args){
+		StringBuilder sb  = new StringBuilder();
+		sb.append("Hi ");
+		sb.append(" How are you?");
+		System.out.println(sb.toString());
+		System.out.printf("this is integer:%d\n",10);
+		System.out.printf("this is float:%f\n",2.456);
+		System.out.printf("this is string:%S\n","hai");
+	}
+}
