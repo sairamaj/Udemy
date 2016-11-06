6d2f5028e4ce0d404f2d6cc6327e974e383c118b
sairama <sairamaj@hotmail.com>
2016-11-06 14:54:20 -0800

    Packages

---
+++ Chap27(Packages)/Application.java
@@ -0,0 +1,9 @@
+import ocean.*;
+import ocean.plant.*;
+
+public class Application{
+	public static void main(String[] args){
+		Fish f = new Fish();
+		Allege a = new Allege();		
+	}
+}
---
+++ Chap27(Packages)/ocean/plant/Allege.java
@@ -0,0 +1,7 @@
+package ocean.plant;
+
+public class Allege{
+	public Allege(){
+		System.out.println("Allege ctor...");
+	}
+}
